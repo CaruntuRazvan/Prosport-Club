38aeaf2ca9c445a7ca55a7af5f349d295f964186
Caruntu Razvan <115624498+CaruntuRazvan@users.noreply.github.com>
2025-05-28 00:20:19 +0300

    Disabled cron jobs during tests using ENABLE_CRON env variable

--- backend/app.js
+++ backend/app.js
@@ -76,9 +76,12 @@ app.get('/api/quote', async (req, res) => {
   }
 });
 
-
-//updateEventStatus(); // actualizarea statusului evenimentelor
-cleanOldNotifications(); // ștergerea notificărilor vechi
-backupMongoDBMonthly(); // backup zilnic al bazei de date
-cleanOldBackups(); // ștergerea backup-urilor vechi
+if (process.env.ENABLE_CRON === 'true') {
+  updateEventStatus(); // actualizarea statusului evenimentelor
+  cleanOldNotifications(); // ștergerea notificărilor vechi
+  backupMongoDBMonthly(); // backup zilnic al bazei de date
+  cleanOldBackups(); // ștergerea backup-urilor vechi
+} else {
+  console.log('Cron jobs are disabled.');
+}
 module.exports = app; 
